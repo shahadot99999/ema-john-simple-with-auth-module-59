18a7233c5b457999b86ccc00c20011320a884774
shahadot99999 <shahadot99999@gmail.com>
2022-10-18 01:45:52 +0600

    again

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Inventory from './components/Inventory/Inventory';
 import { productsAndCartLoader } from './loaders/productsAndCartLoader';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUP/SignUp';
+import Shipping from './components/Shipping/Shipping';
+import PrivateRoute from './routes/PrivateRoute';
 
 
 function App() {
@@ -28,7 +30,11 @@ function App() {
         },
         {
           path: 'inventory',
-          element: <Inventory></Inventory>
+          element: <PrivateRoute><Inventory></Inventory></PrivateRoute>
+        },
+        {
+          path: 'shipping',
+          element: <PrivateRoute><Shipping></Shipping></PrivateRoute>
         },
         {
           path: 'about',
---
+++ src/components/Shipping/Shipping.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Shipping = () => {
+    return (
+        <div>
+            <h2>Shipping</h2>
+            <p>Delivery on the way</p>
+            <p><small>Comming Soon...</small></p>
+        </div>
+    );
+};
+
+export default Shipping;
